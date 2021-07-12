0eb056d950b39881a35dd3717f6f65a4f1832e1a
26ANSH <anshvidyabhanu8@gmail.com>
2021-07-12 20:26:44 +0530

    First Commit

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from auth import new_user, sign_in
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+@app.route('/user')
+def success():
+    return render_template('success.html')
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        call = new_user(email, password)
+        if call == 0:
+            return redirect("/user")
+        else:
+            return render_template('signin.html',color='danger', error=call)
+    else:
+        return render_template('signin.html',color='success',error='Signin with valid Email ID')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        call = sign_in(email, password)
+        if call == 0:
+            return redirect("/user")
+        else:
+            return render_template('login.html',color='danger', error=call)
+    else:
+        return render_template('login.html',color='success', error='Login with valid Email ID')
 
 def error404(e):
     return render_template('404.html')
