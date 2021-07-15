8d8dcb97388fedb0ac668411aa716f031b05fe8e
26ANSH <anshvidyabhanu8@gmail.com>
2021-07-15 22:50:58 +0530

    notes functioning

--- static/script.js
+++ static/script.js
@@ -1,10 +1,17 @@
 function number_of_notes(change) {
     document.getElementById('note-num').innerHTML = parseInt(document.getElementById('note-num').innerHTML) + change
 
-    if (parseInt(document.getElementById('note-num').innerHTML) < 0){
+    if (parseInt(document.getElementById('note-num').innerHTML) < 0) {
         document.getElementById('note-num').innerHTML = 0
     }
 
+}
+
+function note_update(color, text){
+    document.getElementById('note-updates').innerHTML = `<div class="alert alert-${color} alert-dismissible fade show" role="alert">
+    ${text}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" style="font-size:2vh;" aria-label="Close"></button>
+  </div>` + document.getElementById('note-updates').innerHTML
 }
 
 function deletenote(noteid) {
@@ -13,6 +20,7 @@ function deletenote(noteid) {
         body: JSON.stringify({ noteid: noteid }),
     }).then((_res) => {
         number_of_notes(-1)
+        note_update('danger', 'Note Deleted Successfully')
     });
 }
 
@@ -33,13 +41,14 @@ function newnote() {
             body: JSON.stringify({ note_data: note }),
         }).then((_res) => {
             number_of_notes(1)
+            note_update('success', 'Note Successfully Added')
             _res.json().then(data => {
                 notes = `<div class='alert alert-secondary alert-dismissible fade show text-dark' role='alert'>
                 ` + note + `
                 <button type='button' onclick="deletenote('${data.noteid}')" class='btn-close' data-bs-dismiss='alert'
                   aria-label='Close'></button>
                 </div>`
-                    document.getElementById('notes_section').innerHTML += notes
+                document.getElementById('notes_section').innerHTML += notes
             });
         });
     }
