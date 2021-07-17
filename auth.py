609a2028242b0e2c2fad67bd7d80a9812f46ead2
26ANSH <anshvidyabhanu8@gmail.com>
2021-07-17 22:34:23 +0530

    Completed

--- auth.py
+++ auth.py
@@ -2,14 +2,17 @@ import pyrebase
 import requests
 import json
 
+# Please Follow Detailed Explanation from Readme
+# Paste your Friebase Details here
+# !!! VERY IMPORTANT !!!
 CONFIG = {
-    "apiKey" : "AIzaSyAGXobNG-hC6ufs4eV-ca1KMo5k-FEAusA",
-    "authDomain" : "firecatchers-task-2.firebaseapp.com",
-    "databaseURL" : "https://firecatchers-task-2-default-rtdb.firebaseio.com",
-    "projectId" : "firecatchers-task-2",
-    "storageBucket" : "firecatchers-task-2.appspot.com",
-    "messagingSenderId" : "664983494868",
-    "appId" : "1:664983494868:web:e4eb6e31cb509f03535b41"
+    "apiKey" : "",
+    "authDomain" : "",
+    "databaseURL" : "",
+    "projectId" : "",
+    "storageBucket" : "",
+    "messagingSenderId" : "",
+    "appId" : ""
   }
 
 firebase = pyrebase.initialize_app(CONFIG)
